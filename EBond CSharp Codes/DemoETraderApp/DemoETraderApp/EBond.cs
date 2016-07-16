6613490f682faf827a6d490a978ba66cdf5b5e5b
Sreenivaspiitr <sreenivaspiitr@gmail.com>
2016-07-16 16:54:41 +0530

    Updated GUI with 1st window

---
+++ EBond CSharp Codes/DemoETraderApp/DemoETraderApp/EBond.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace DemoETraderApp
+{
+    [DataContract]
+   public class EBond
+    {
+        [DataMember]
+        public int bondId { get; set; }
+        [DataMember]
+        public string isin { get; set; }
+        [DataMember]
+        public string issueDate { get; set; }
+        [DataMember]
+        public string settlementDays { get; set; }
+        [DataMember]
+        public int faceValue { get; set; }
+        [DataMember]
+        public double couponRate { get; set; }
+        [DataMember]
+        public char couponFrequency { get; set; }
+        [DataMember]
+        public string maturityDate { get; set; }
+        [DataMember]
+        public double lastPrice { get; set; }
+        [DataMember]
+        public double high { get; set; }
+        [DataMember]
+        public double low { get; set; }
+        [DataMember]
+        public double changeInPrice { get; set; }
+        [DataMember]
+        public double yeild { get; set; }
+        [DataMember]
+        public string creditRating { get; set; }
+        [DataMember]
+        public string currency { get; set; }
+
+        public EBond(string isin, string issueDate, string settlementDays, int faceValue, double couponRate,
+            char couponFrequency, string maturityDate, double lastPrice, double high, double low, double changeInPrice,
+            double yeild, string creditRating, string currency)
+        {
+            
+            this.isin = isin;
+            this.issueDate = issueDate;
+            this.settlementDays = settlementDays;
+            this.faceValue = faceValue;
+            this.couponRate = couponRate;
+            this.couponFrequency = couponFrequency;
+            this.maturityDate = maturityDate;
+            this.lastPrice = lastPrice;
+            this.high = high;
+            this.low = low;
+            this.changeInPrice = changeInPrice;
+            this.yeild = yeild;
+            this.creditRating = creditRating;
+            this.currency = currency;
+        }
+
+        public override string ToString()
+        {
+            return string.Format(isin);
+        }
+    }
+}
--- EBond CSharp Codes/DemoETraderApp/DemoETraderApp/MainWindow.xaml.cs
+++ EBond CSharp Codes/DemoETraderApp/DemoETraderApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
 
 namespace DemoETraderApp
 {
@@ -26,46 +30,121 @@ namespace DemoETraderApp
         {
             InitializeComponent();
         }
-
+        
         private void ShowBSM(object sender, RoutedEventArgs e)
         {
            // Show();
         }
 
-        private void ListSetup(object sender, RoutedEventArgs e)
-        {
-            Bondslstbx.Items.Add("AB Stock");
-            Bondslstbx.Items.Add("AE Stock");
-            Bondslstbx.Items.Add("AD Stock");
-            Bondslstbx.Items.Add("AC Stock");
-        }
+        public List<EBond> EBondList = new List<EBond>();
+        List<EBond> BondList = new List<EBond>();
 
-        private List<BondClass> bondsetup()
+        private List<EBond> bondsetup(EBond obj)
         {
-            List<BondClass> BondList = new List<BondClass>();
-            BondClass ABStockObj = new BondClass(1, "Isin1", DateTime.Today, "status", 100, 105, 102, "profit", 5);
-            BondList.Add(ABStockObj);
+            BondList = new List<EBond>();
+            
+            BondList.Add(obj);
             return BondList;
         }
         private void selectbond(object sender, RoutedEventArgs e)
         {
-
+            //int index = Bondslstbx.SelectedIndex;
             
-            if (Bondslstbx.SelectedIndex == 0)
+            if (Bondslstbx.SelectedIndex == -1)
             {
-                
-                dataGrid.ItemsSource = bondsetup();
+                MessageBox.Show("Please select a bond");               
             }
-            buttonfrBT.IsEnabled = true;
+            else
+            {
+                EBond sentebond = (EBond)Bondslstbx.SelectedItem;
+               dataGridfrbonds.ItemsSource= bondsetup(sentebond);
+               //dataGridfrbonds.Items.Add(sentebond);
+                buttonfrBT.IsEnabled = true;
+            }
+            
         }
 
         private void OpenTBWTab(object sender, RoutedEventArgs e)
         {
-            
+           
             tabControl.SelectedItem = TBWTab;
             // Load events...(later..)
            
             
         }
+
+        private void BondListSetup(object sender, RoutedEventArgs e)
+        {
+            // Bondlist Setup
+            // string msg = "{\"isin\":\"IN126\",\"bondName\":\"saleem\"}";
+            // MessageBox.Show(CRfrom.Text);
+           
+            string mdto = MDto.SelectedDate.Value.ToShortDateString();
+            string searchstring = "{\"isin\":\"" + txtbxisin.Text + "\",\"couponRateFrom\":\"" + CRfrom.Text +
+                "\",\"couponRateTo\":\"" + CRto.Text + "\",\"frequency\":\"" + comboBoxFrequency.SelectedItem.ToString() +
+                "\",\"currency\":\"" + comboBox1.SelectedItem.ToString() + "\",\"creditRating\":\"" + CreditRatingBx.SelectedItem.ToString() +
+                "\",\"yeildFrom\":\"" + txtbxYield.Text+ "\",\"yeildTo\":\"" + yeildTo.Text + "\",\"lastPriceFrom\":\"" +
+                LPfrom.Text+ "\",\"lastPriceTo\":\"" + LPto.Text + "\",\"maturityDateFrom\":\"" +MDfrom.SelectedDate.Value.ToShortDateString()
+                + "\",\"maturityDateTo\":\"" + mdto + "\"}";
+            // MessageBox.Show(searchstring);
+            var cli = new WebClient();
+            cli.Headers[HttpRequestHeader.ContentType] = "text/plain";
+            string send = cli.UploadString("http://192.168.137.37:8080/EBondTraderWeb/rest/bond/bsm", searchstring);
+
+            //MessageBox.Show(send);
+
+            //string response = cli.DownloadString("http://192.168.137.37:8080/EBondTraderWeb/rest/bond");
+            Stream Ebondstream = StringToStream(send);
+            StreamReader reader = new StreamReader(Ebondstream);
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(EBond[]));
+            EBond[] EBonds = (EBond[])serializer.ReadObject(Ebondstream);
+            //string help = "";
+            Bondslstbx.Items.Clear();
+            dataGridfrbonds.ItemsSource = null;
+            foreach(EBond ebond in EBonds)
+            {
+                Bondslstbx.Items.Add(ebond);
+                // help += ebond.ToString()+ "\n";
+               
+            }
+           // MessageBox.Show(help);
+
+        }
+
+        private void Setup(object sender, RoutedEventArgs e)
+        {
+            CreditRatingBx.Items.Add("");
+            CreditRatingBx.Items.Add("AAA");
+            CreditRatingBx.Items.Add("AA");
+            CreditRatingBx.Items.Add("A");
+            CreditRatingBx.Items.Add("BBB");
+            CreditRatingBx.Items.Add("BB");
+            CreditRatingBx.Items.Add("B");
+            CreditRatingBx.Items.Add("CCC");
+            CreditRatingBx.Items.Add("CC");
+            CreditRatingBx.Items.Add("C");
+            comboBoxFrequency.Items.Add("");
+            comboBoxFrequency.Items.Add("A");
+            comboBoxFrequency.Items.Add("S");
+            comboBoxFrequency.Items.Add("Q");
+            comboBox1.Items.Add("");
+            comboBox1.Items.Add("USD");
+            comboBox1.Items.Add("INR");
+            comboBox1.Items.Add("GBP");
+            comboBox1.Items.Add("EUR");
+
+        }
+        public Stream StringToStream(string s)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+
+        }
+
+       
     }
 }
