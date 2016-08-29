fed218079ca631c8a86294b345bbaa241ec8ea74
Shashwat Pulak <shashwat.pulak@gmail.com>
2016-08-29 10:26:32 +0530

    Updated Bond Manager

--- EBondTraderEJB/ejbModule/ebond/trader/ejb/BondManager.java
+++ EBondTraderEJB/ejbModule/ebond/trader/ejb/BondManager.java
@@ -2,7 +2,7 @@ package ebond.trader.ejb;
 
 import java.util.List;
 
-import javax.ejb.Stateless;
+import javax.ejb.*;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
@@ -10,6 +10,8 @@ import javax.persistence.TypedQuery;
 import ebond.trader.jpa.Bond;
 
 //This is an session bean
+@Remote(BondManagerRemote.class)
+@Local(BondManagerLocal.class)
 @Stateless
 public class BondManager implements BondManagerRemote, BondManagerLocal {
 
@@ -33,5 +35,111 @@ public class BondManager implements BondManagerRemote, BondManagerLocal {
 
 		return (List<Bond>)query.getResultList();
     }
+    
+    public List<Bond> getBondResultSet(String isin, String creditRating, String couponRateFrom, String couponRateTo,
+			String maturityDateFrom, String maturityDateTo, String frequency, String currency) {
+
+		String tempQuery = "SELECT b from Bond AS b ";// trailing spaces added
+														// to prevent accidental
+														// concat
+		int notNullCount = -1;
+
+		// blank query case
+		if (isin.length() == 0 && creditRating.length() == 0 && couponRateFrom.length() == 0
+				&& couponRateTo.length() == 0 && maturityDateFrom.length() == 0 && maturityDateTo.length() == 0
+				&& frequency.length() == 0 && currency.length() == 0) {
+			System.out.println("Search is null");
+
+		} else {
+
+			tempQuery = tempQuery + " WHERE ";// if any entry is not null, we
+												// need a WHERE clause first
+			notNullCount++;// this is a reference to check whether to add AND or
+							// not
+			// begin with adding
+			// ISIN_________________________________________________
+			if (isin.length() != 0 && notNullCount == 0) {
+				tempQuery = tempQuery + "b.isin=" + isin;
+				notNullCount++;// any non null String after this will have an
+								// AND clause before it
+			}
+			// adding creditRating__________________________________
+			if (creditRating.length() != 0 && notNullCount == 0) {
+				// if isin is null,notNullCount is still 0 we proceed after
+				// WHERE
+				tempQuery = tempQuery + "b.creditRating=" + creditRating;
+				notNullCount++;// any non null String after this will have an
+								// AND clause before it
+
+			} else if (creditRating.length() != 0 && notNullCount > 0) {
+				// if isin is not null,notNullCount>0 we add an AND clause in
+				// between
+				tempQuery = tempQuery + " AND ";// safety spaces added
+				tempQuery = tempQuery + "b.creditRating=" + creditRating;
+				notNullCount++;
+
+			}
+			// adding couponRateFrom and To__________________________________
+			if (couponRateFrom.length() != 0 && couponRateTo.length()!=0 && notNullCount == 0) {
+				tempQuery = tempQuery + "b.couponRate BETWEEN " + couponRateFrom + " AND " + couponRateTo;
+				notNullCount++;
+
+			} else if (couponRateFrom.length() != 0 && couponRateTo.length()!=0 && notNullCount > 0) {
+				tempQuery = tempQuery + " AND ";// safety spaces added
+				tempQuery = tempQuery + "b.couponRate BETWEEN " + couponRateFrom + " AND " + couponRateTo;
+				notNullCount++;
+
+			}
+			// adding maturityDateFrom and To__________________________________
+			if (maturityDateFrom.length() != 0 && maturityDateTo.length() != 0 && notNullCount == 0) {
+				tempQuery = tempQuery + "b.maturityDate BETWEEN " + maturityDateFrom + " AND " + maturityDateTo;
+				notNullCount++;
+
+			} else if (maturityDateFrom.length() != 0 && maturityDateTo.length() != 0 && notNullCount > 0) {
+				tempQuery = tempQuery + " AND ";// safety spaces added
+				tempQuery = tempQuery + "b.maturityDate BETWEEN " + maturityDateFrom + " AND " + maturityDateTo;
+				notNullCount++;
+
+			}
+
+			// adding frequency__________________________________
+			if (frequency.length() != 0 && notNullCount == 0) {
+				// if isin is null,notNullCount is still 0 we proceed after
+				// WHERE
+				tempQuery = tempQuery + "b.frequency=" + frequency;
+				notNullCount++;// any non null String after this will have an
+								// AND clause before it
+
+			} else if (frequency.length() != 0 && notNullCount > 0) {
+				// if isin is not null,notNullCount>0 we add an AND clause in
+				// between
+				tempQuery = tempQuery + " AND ";// safety spaces added
+				tempQuery = tempQuery + "b.couponFrequency=" + frequency;
+				notNullCount++;
+
+			}
+			// adding currency__________________________________
+			if (currency.length() != 0 && notNullCount == 0) {
+				// if isin is null,notNullCount is still 0 we proceed after
+				// WHERE
+				tempQuery = tempQuery + "b.currency=" + currency;
+				notNullCount++;// any non null String after this will have an
+								// AND clause before it
+
+			} else if (currency.length() != 0 && notNullCount > 0) {
+				// if isin is not null,notNullCount>0 we add an AND clause in
+				// between
+				tempQuery = tempQuery + " AND ";// safety spaces added
+				tempQuery = tempQuery + "b.currency=" + currency;
+				notNullCount++;
+
+			}
+		}
+		System.out.println("Executed: " + tempQuery);
+		tempQuery = "SELECT b from Bond AS b ";// DELETE THIS LINE
+		TypedQuery<Bond> query = em.createQuery(tempQuery, Bond.class);
+
+		return (List<Bond>) query.getResultList();
+	}
 
 }
